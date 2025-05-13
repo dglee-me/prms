b718c980ddcef71d39e6395434dceb574603dd48
dglee-me <dglee.dev@gmail.com>
2025-05-13 16:00:14 +0900

    initialize

---
+++ src/main/java/kr/co/dglee/system/PrmsApplication.java
@@ -0,0 +1,13 @@
+package kr.co.dglee.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/dglee/system/PrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.dglee.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
